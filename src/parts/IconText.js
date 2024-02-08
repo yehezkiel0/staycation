dc5a4eb9abc53f8f34a9840b979c90bd23b0116f
Yehezkiel Imannuel <91413725+yehezkiel0@users.noreply.github.com>
2024-02-08 12:28:32 +0700

    Create Header component and put arrange LandingPage

---
+++ src/parts/IconText.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Button from "elements/Button";
+export default function IconText() {
+  return (
+    <Button className="brand-text-icon" href="" type="link">
+      Stay<span className="text-secondary">cation.</span>
+    </Button>
+  );
+}
